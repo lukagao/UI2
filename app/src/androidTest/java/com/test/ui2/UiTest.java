e7a084dc2738381cddfe58a48de014f42ab00387
lukagao <790794090@qq.com>
2019-10-17 17:44:06 +0800

    no matter

--- app/src/androidTest/java/com/test/ui2/UiTest.java
+++ app/src/androidTest/java/com/test/ui2/UiTest.java
@@ -1,15 +1,11 @@
 package com.test.ui2;
 
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObjectNotFoundException;
-
 import java.io.IOException;
-
 import com.test.ui2.core.MySocket;
 import com.test.ui2.core.Server;
 
@@ -20,9 +16,6 @@ public class UiTest {
     private Server server;
     @Test
     public void run() throws UiObjectNotFoundException,IOException {
-
-        //sock=new MySocket();
-        //sock.start();
         //Choreographer.getInstance().postFrameCallback(new FPSFrameCallback());
         server=new Server();
         server.start();
---
+++ app/src/androidTest/java/com/test/ui2/utils/MD5Utils.java
@@ -0,0 +1,121 @@
+package com.test.ui2.utils;
+
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class MD5Utils {
+
+    private static final int STREAM_BUFFER_LENGTH = 1024;
+
+    public static MessageDigest getDigest(final String algorithm) throws NoSuchAlgorithmException {
+        return MessageDigest.getInstance(algorithm);
+    }
+
+    public static byte[] md5(String txt) {
+        return md5(txt.getBytes());
+    }
+
+    public static byte[] md5(byte[] bytes) {
+        try {
+            MessageDigest digest = getDigest("MD5");
+            digest.update(bytes);
+            return digest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static byte[] md5(InputStream is) throws NoSuchAlgorithmException, IOException {
+        return updateDigest(getDigest("MD5"), is).digest();
+    }
+
+    public static MessageDigest updateDigest(final MessageDigest digest, final InputStream data) throws IOException {
+        final byte[] buffer = new byte[STREAM_BUFFER_LENGTH];
+        int read = data.read(buffer, 0, STREAM_BUFFER_LENGTH);
+        while (read > -1) {
+            digest.update(buffer, 0, read);
+            read = data.read(buffer, 0, STREAM_BUFFER_LENGTH);
+        }
+        return digest;
+    }
+
+    //写一个md5加密的方法
+    public static String md5String(String plainText) {
+        if (TextUtils.isEmpty(plainText)) return "";
+        //定义一个字节数组
+        byte[] secretBytes = null;
+        try {
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //对字符串进行加密
+            md.update(plainText.getBytes());
+            //获得加密后的数据
+            secretBytes = md.digest();
+        } catch (Exception e) {
+            return plainText;
+        }
+        //将加密后的数据转换为16进制数字
+        String md5code = new BigInteger(1, secretBytes).toString(16);// 16进制数字
+        // 如果生成数字未满32位，需要前面补0
+        for (int i = 0; i < 32 - md5code.length(); i++) {
+            md5code = "0" + md5code;
+        }
+        return md5code;
+    }
+
+    public static String getFileMD5(String localPath) {
+        if (TextUtils.isEmpty(localPath)) return "";
+        File file = new File(localPath);
+        if (file.exists())
+            return getFileMD5(file);
+        else
+            return "";
+    }
+
+    public static String getFileMD5(File file) {
+        if (!file.isFile()) {
+            return "";
+        }
+        MessageDigest digest = null;
+        FileInputStream in = null;
+        byte[] buffer = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+        return bytesToHexString(digest.digest());
+    }
+
+    public static String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (src == null || src.length <= 0) {
+            return "";
+        }
+        for (byte aSrc : src) {
+            int v = aSrc & 0xFF;
+            String hv = Integer.toHexString(v);
+            if (hv.length() < 2) {
+                stringBuilder.append(0);
+            }
+            stringBuilder.append(hv);
+        }
+        return stringBuilder.toString();
+    }
+}
